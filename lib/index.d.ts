4585f1ce47a6a25f4d86b7f6dd37b9b6600e8ab6
michitaro <michitaro.nike@gmail.com>
2017-09-20 09:00:04 +0900

    cleanup

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class MagicTrackpadDetector {
-    tolerance: number;
-    interval: number;
-    minN1: number;
-    minN2: number;
-    private history;
-    inertial(e: WheelEvent): boolean;
-}
